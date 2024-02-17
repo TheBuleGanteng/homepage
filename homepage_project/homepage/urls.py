aee742cea442c7fe8481f371711409f9fbb2ade2
thebuleganteng <mcdonnell.matthew@ymail.com>
2024-02-18 06:49:29 +0800

    Additional work on refomatting layout.html and index.html

--- homepage_project/homepage/urls.py
+++ homepage_project/homepage/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+# Defines application's namespace (set in project's urls.py file)
+app_name = 'homepage'
+
 urlpatterns = [
-    path("", views.index, name='homepage_index')
+    path("", views.index, name='index')
     
 ]
